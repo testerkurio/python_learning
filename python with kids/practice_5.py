8823e3eb2379dc506285283df5d8d35462e13172
kurio <kuriollr@163.com>
2016-11-16 12:53:05 +0800

    第五章练习题-修改注释

--- python with kids/practice_5.py
+++ python with kids/practice_5.py
@@ -3,24 +3,30 @@ a = 'Liu'
 b = 'Lirong'
 print(a+b)
 
+'''
 #2
-#a = input("what's your family name?")
-#b = input("what's your name?")
-#print('your name are:',a+b)
+a = input("what's your family name?")
+b = input("what's your name?")
+print('your name are:',a+b)
+'''
 
+'''
 #3
-#a = float(input("tell me the room's length is:"))
-#b = float(input("tell me the room's width is:"))
-#c = a*b
+a = float(input("tell me the room's length is:"))
+b = float(input("tell me the room's width is:"))
+c = a*b
 #print("the room need:",c,"sq.m. rugs.")
+'''
 
+'''
 #4
-#d = float(input("tell me the sq.feet rug's price is:"))
-#e = 9*c
-#f = e*d
-#print("ALL need:",c,"sq.m. rugs.")
-#print("ALL need:",e,"sq.feet rugs.")
-#print("ALL price are:",f,"yuan")
+d = float(input("tell me the sq.feet rug's price is:"))
+e = 9*c
+f = e*d
+print("ALL need:",c,"sq.m. rugs.")
+print("ALL need:",e,"sq.feet rugs.")
+print("ALL price are:",f,"yuan")
+'''
 
 #5
 five = int(input("有几个五分钱："))
